91d18d1251502b26b2db739b9e788bd65eda5201
RawanKawtharanii <RawanKawtharanii@users.noreply.github.com>
2022-11-03 14:33:32 +0000

    ✅ Ready to clone and code.

--- generatedoc/cli.py
+++ generatedoc/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for generatedoc project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m generatedoc` and `$ generatedoc `.
 
     This is your program's entry point.
 
